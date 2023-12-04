4219f4d44de9587827b3060098cb70b72731dab1
VensanDrot <vensandrot@gmail.com>
2023-12-03 19:24:50 -0500

    gtag

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -32,9 +32,22 @@ export default async function LocaleLayout({
     console.log(error);
   }
 
-  return (
+  https: return (
     <html lang={locale}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TSXS88M6'`,
+          }}
+        />
+      </head>
       <body className={inter.className} suppressHydrationWarning={true}>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSXS88M6" height="0" width="0"></iframe>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Nav />
           {children}
