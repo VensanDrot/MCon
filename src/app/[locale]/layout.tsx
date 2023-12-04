9b820ca0dcc0eeb1949047850f5c4cb45c40c2e7
VensanDrot <vensandrot@gmail.com>
2023-12-03 23:37:47 -0500

    g tag 1.3

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -37,16 +37,18 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <head>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`} />;
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TSXS88M6'`,
-          }}
-        />
+        <script src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`} />
+
+        <script id="ga-script">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GTM_ID}', {
+      page_path: window.location.pathname,
+    });
+        `}
+        </script>
       </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         <noscript>
