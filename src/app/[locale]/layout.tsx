dcd926d40dfb86d2783bf1ef7f002fe7672e90a6
vensandrot <vensandrot@gmail.com>
2025-08-18 17:28:54 -0400

    pixel integration

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,13 @@
 import { NextIntlClientProvider } from "next-intl";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import Nav from "@/components/Nav";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
+
 export function generateStaticParams() {
   return [{ locale: "en" }, { locale: "ru" }];
 }
@@ -34,7 +36,40 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body className={inter.className} suppressHydrationWarning>
+        {/* Load Meta Pixel once the page is interactive */}
+        {process.env.NODE_ENV === "production" && (
+          <>
+            <Script
+              id="facebook-pixel"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '1817017441772951');
+                  fbq('track', 'PageView');
+                `,
+              }}
+            />
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src="https://www.facebook.com/tr?id=1817017441772951&ev=PageView&noscript=1"
+                alt=""
+              />
+            </noscript>
+          </>
+        )}
+
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Nav />
           {children}
